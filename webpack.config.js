c3c1bb34c62f2f173971554fefff4cfd4aebb764
guansss <821143943@qq.com>
2020-01-10 21:48:20 +0800

    Make framework

---
+++ webpack.config.js
@@ -0,0 +1,69 @@
+const path = require('path');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+const { CleanWebpackPlugin } = require('clean-webpack-plugin');
+const ForkTsCheckerWebpackPlugin = require('fork-ts-checker-webpack-plugin');
+
+const config = {
+    entry: './src/index.ts',
+    devServer: {
+        contentBase: './dist',
+        hot: true,
+    },
+    module: {
+        rules: [
+            {
+                test: /\.ts$/,
+                use: [
+                    {
+                        loader: 'ts-loader',
+                        options: {
+                            transpileOnly: true,
+                        },
+                    },
+                ],
+                exclude: /node_modules/,
+            },
+            {
+                test: /\.css$/,
+                use: [
+                    'style-loader',
+                    'css-loader',
+                ],
+            },
+            {
+                test: /\.(png|svg|jpg|gif)$/,
+                use: [
+                    'file-loader',
+                ],
+            },
+        ],
+    },
+    plugins: [
+        new CleanWebpackPlugin(),
+        new ForkTsCheckerWebpackPlugin(),
+        new HtmlWebpackPlugin({
+            template: 'src/index.html',
+        }),
+    ],
+    resolve: {
+        alias: {
+            '@': '/src',
+        },
+        extensions: ['.ts', '.js'],
+    },
+    output: {
+        publicPath: '', // use empty path to make generated files be able to load by 'file://' scheme
+        filename: '[name].[hash:4].js',
+        path: path.resolve(__dirname, 'dist'),
+    },
+};
+
+module.exports = (env, argv) => {
+    config.mode = argv.mode;
+
+    if (argv.mode === 'development') {
+        config.devtool = 'inline-source-map';
+    }
+
+    return config;
+};
