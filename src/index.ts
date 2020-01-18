a4313179c8b02dd836ac1b76dc0bb25775eb3468
guansss <821143943@qq.com>
2020-01-18 23:03:43 +0800

    Remove stats in production

--- src/index.ts
+++ src/index.ts
@@ -2,7 +2,6 @@ import { VIEW_DISTANCE } from '@/constants';
 import Mka from '@/mka/Mka';
 import Ticker from '@/mka/Ticker';
 import MotionPlayer from '@/players/MotionPlayer';
-import StatsPlayer from '@/players/StatsPlayer';
 import TilesPlayer from '@/players/TilesPlayer';
 import Postprocessing from '@/rendering/Postprocessing';
 import { init as initWallpaper } from '@/we/WallpaperHelper';
@@ -21,6 +20,11 @@ camera.position.z = 5;
 const postprocessing = new Postprocessing(renderer, scene, camera);
 
 const mka = new Mka();
+
+function initMka() {
+    mka.addPlayer('tiles', new TilesPlayer(scene, camera));
+    mka.addPlayer('motion', new MotionPlayer(camera));
+}
 
 let initialized = false;
 
@@ -56,10 +60,7 @@ mka.on('afterUpdate', () => postprocessing.render(Ticker.delta))
     .on('we:*', (name: string, value: any, initial?: boolean) => {
         if (!initialized && initial) {
             initialized = true;
-
-            mka.addPlayer('tiles', new TilesPlayer(scene, camera));
-            mka.addPlayer('motion', new MotionPlayer(camera));
-            mka.addPlayer('stats', new StatsPlayer());
+            initMka();
         }
     });
 
